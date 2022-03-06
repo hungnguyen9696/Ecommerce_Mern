ab6f643d89a05d055de74304596bd1c0d6743d0d
hung nguyen <hungnvh96@gmail.com>
2022-03-06 21:42:13 +0200

    first commit

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,56 +1,53 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const expressValidator = require('express-validator');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 // import routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree');
-const orderRoutes = require('./routes/order');
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
 // app
 const app = express();
 
 // db
-mongoose.connect(
-    process.env.MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    }
-)
-    .then(() => console.log('DB Connected'));
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`)
+mongoose.connection.on("error", (err) => {
+	console.log(`DB connection error: ${err.message}`);
 });
 
 // middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
 // routes middleware
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', braintreeRoutes);
-app.use('/api', orderRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
 
 // will redirect all the non-api routes to react frontend
